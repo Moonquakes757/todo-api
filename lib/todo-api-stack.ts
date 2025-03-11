ffe6de15eb2a800567b20ea183dc2881a8a42433
Moonquakes757 <xsh20020902@gmail.com>
2025-03-11 19:48:31 +0000

    Building Infrastructure (CDK Stack)

--- lib/todo-api-stack.ts
+++ lib/todo-api-stack.ts
@@ -1,16 +1,81 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 
 export class TodoApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create DynamoDB table with composite primary keys: userId (Partition Key) and todoId (Sort Key)
+    const table = new dynamodb.Table(this, 'TodoTable', {
+      partitionKey: { name: 'userId', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'todoId', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TodoApiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create a Lambda function, the code is located in the lambda/directory, and the entry function is index.handler
+    const todoFunction = new lambda.Function(this, 'TodoFunction', {
+      runtime: lambda.Runtime.NODEJS_18_X,  
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'index.handler',
+      environment: {
+        TABLE_NAME: table.tableName,
+      },
+    });
+
+    // Granting read and write permissions to Lambda functions on DynamoDB tables
+    table.grantReadWriteData(todoFunction);
+
+    // Create API Gateway REST API and integrate it with Lambda
+    const api = new apigateway.RestApi(this, 'TodoApi', {
+      restApiName: 'ToDo Service',
+      description: 'This service serves a ToDo management API.',
+    });
+
+    // Create API Key to protect POST and PUT requests
+    const apiKey = api.addApiKey('ApiKey');
+
+    // Define a usage plan and associate the API Key with the plan
+    const plan = api.addUsagePlan('UsagePlan', {
+      name: 'Easy',
+      apiKey,
+      throttle: {
+        rateLimit: 10,
+        burstLimit: 2,
+      },
+    });
+    plan.addApiStage({
+      stage: api.deploymentStage,
+    });
+
+    // Define Lambda Integration
+    const lambdaIntegration = new apigateway.LambdaIntegration(todoFunction);
+
+    // Define resources and methods
+
+    // /todos resource
+    const todosResource = api.root.addResource('todos');
+    
+    // GET /todos/{userId}: Query all to-do items of the specified user, optionally with a query string for filtering
+    const userResource = todosResource.addResource('{userId}');
+    userResource.addMethod('GET', lambdaIntegration); // API Key not need here
+
+    // POST /todos: Add to-do list (requires API Key)
+    todosResource.addMethod('POST', lambdaIntegration, {
+      apiKeyRequired: true,
+    });
+
+    // PUT /todos/{userId}/{todoId}: Update to-do list (requires API Key)
+    const singleTodoResource = userResource.addResource('{todoId}');
+    singleTodoResource.addMethod('PUT', lambdaIntegration, {
+      apiKeyRequired: true,
+    });
+
+    // GET /todos/{userId}/{todoId}/translation: Return the translation result of the specified to-do item description
+    const translationResource = singleTodoResource.addResource('translation');
+    translationResource.addMethod('GET', lambdaIntegration); // Translation interface does not require API Key
   }
 }
+
